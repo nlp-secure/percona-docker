51c288b85241de018619540f1a7d69be3459ce51
Israel Shirk <israelshirk@gmail.com>
2018-07-31 22:02:56 -0600

    Fix typo in configure-pmm, up wait on pmm-client

--- percona-xtradb-57/dockerdir/usr/bin/configure-pmm.sh
+++ percona-xtradb-57/dockerdir/usr/bin/configure-pmm.sh
@@ -6,4 +6,4 @@ sleep 10
 
 pmm-admin config -c "$DATADIR/pmm.yaml" --server pmm-server --server-insecure-ssl --server-user "$PMM_CLIENT_USER" --server-password "$PMM_CLIENT_PASSWORD"
 pmm-admin add mysql:metrics -c "$DATADIR/pmm.yaml" --user="$MYSQL_MONITOR_USERNAME" --password="$MYSQL_MONITOR_PASSWORD"
-pmm-admin add mysql:queries -c "$DATADIR/pmm.yaml" --user="$MYSQL_MONITOR_USERNAME" --passowrd="$MYSQL_MONITOR_PASSWORD"
+pmm-admin add mysql:queries -c "$DATADIR/pmm.yaml" --user="$MYSQL_MONITOR_USERNAME" --password="$MYSQL_MONITOR_PASSWORD"
